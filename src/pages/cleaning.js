1353378f5f0533aa77da5ab48ccafd0e3abd5601
MarkMansour1 <markmansour01@gmail.com>
2021-07-01 23:31:26 +0100

    Services

---
+++ src/pages/cleaning.js
@@ -0,0 +1,137 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Banner from "../components/banner"
+
+class PageComponent extends React.Component {
+  render() {
+    const { data } = this.props
+
+    return (
+      <Layout>
+        <SEO title="Cleaning" />
+        <Banner
+          details={[
+            "cleaning",
+            "If you can't find what you're looking for you can contact us to source your desired product and we'll deliver them straight to you.",
+            data.banner.childImageSharp.fluid,
+            false,
+          ]}
+        />
+        <div className="container container-wide pt-5">
+          <h1 className="d-md-none pt-3">Sourcing</h1>
+          <div className="row">
+            <div className="col-md-4">
+              <p className="text-gray">
+                Fill in the form with the details and photos of the pair of
+                sneakers you need cleaned. We will get back to you ASAP with the
+                treatment your sneakers require.
+              </p>
+              <h4>Standard Treatment: £20</h4>
+              <p className="text-gray">
+                Our standard treatment includes a deep upper & midsole clean and
+                under-sole clean including Jordans, Yeezys etc.
+              </p>
+              <h4>Premium Treatment: £25</h4>
+              <p className="text-gray">
+                Our premium treatment includes a deep upper & midsole clean and
+                under-sole clean including luxury designer trainers such as Dior
+                B22’s, Balenciagas etc. Each initial order comes with a pack of
+                free shoe trees.
+              </p>
+            </div>
+            <div className="col-md-1" />
+            <div className="col-md-7 contactform m-0 pl-md-5">
+              <form name="cleaning" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="cleaning" />
+                <div className="form-group">
+                  <label htmlFor="email">Email Address *</label>
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="model">Trainer Model *</label>
+                  <input
+                    type="text"
+                    id="model"
+                    name="model"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlhtmlFor="size">UK size *</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="size"
+                    min="1"
+                    max="13"
+                    step="0.5"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="instagram">
+                    Instagram * (Expect to receive an invoice within two hours)
+                  </label>
+                  <input
+                    type="text"
+                    id="instagram"
+                    name="instagram"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="notes">Notes</label>
+                  <textarea
+                    id="notes"
+                    name="notes"
+                    className="form-control"
+                    rows="5"
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Upload a photo</label>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      className="custom-file-input"
+                      id="attachments"
+                    />
+                    <label
+                      className="custom-file-label"
+                      htmlhtmlFor="attachments"
+                    >
+                      Choose file
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-secondary">
+                  Send Message
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query {
+    banner: file(relativePath: { eq: "banners/cleaning.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
+
+export default PageComponent
--- src/pages/sourcing.js
+++ src/pages/sourcing.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -25,83 +26,206 @@ class PageComponent extends React.Component {
   }
 
   render() {
+    const { data } = this.props
+
+    const reviews = [
+      data.img4,
+      data.img1,
+      data.img2,
+      data.img3,
+      data.img7,
+      data.img9,
+      data.img56,
+      data.img3462,
+    ]
+
     return (
       <Layout>
         <SEO title="Sourcing" />
-        <div className="container pt-5" style={{ maxWidth: "500px" }}>
-          <div className="text-center">
-            <h2>Sourcing</h2>
-            <p className="text-gray mb-4">
-              Looking for a pair of trainers to buy? Fill out the form with the
-              details of the pair you’re looking for. We will get back to you
-              ASAP with a price.
-            </p>
+        <Banner
+          details={[
+            "sourcing",
+            "If you can't find what you're looking for you can contact us to source your desired product and we'll deliver them straight to you.",
+            data.banner.childImageSharp.fluid,
+            false,
+          ]}
+        />
+        <div className="container container-wide pt-5">
+          <h1 className="d-md-none pt-3">Sourcing</h1>
+          <div className="row">
+            <div className="col-md-4">
+              <p className="text-gray">
+                We know it can be quite difficult nowadays to find sold out
+                trainers you’ve been looking all around for, so we solve that
+                problem and source them for you.
+              </p>
+              <h4>The Process is Simple:</h4>
+              <p className="text-gray">
+                You request an item, we find the item that has been requested
+                and then we provide you with a quote for your item. The item
+                will then be posted to the delivery address you give us once
+                payment has been received and you’ll be updated with a tracking
+                reference shortly after.
+              </p>
+              <p>
+                We want you to have the best experience possible so we ensure
+                that we provide fast response, competitive prices and a fast
+                delivery time.
+              </p>
+            </div>
+            <div className="col-md-1" />
+            <div className="col-md-7 contactform m-0 pl-md-5">
+              <form name="sourcing" method="POST" data-netlify="true">
+                <input type="hidden" name="form-name" value="sourcing" />
+                <div className="form-group">
+                  <label htmlhtmlFor="email">Email address *</label>
+                  <input
+                    type="email"
+                    className="form-control"
+                    id="email"
+                    name="email"
+                    value={this.state.email}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlhtmlFor="model">Model (name of trainer) *</label>
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="model"
+                    name="model"
+                    value={this.state.model}
+                    onChange={this.handleChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlhtmlFor="size">UK size *</label>
+                  <input
+                    type="number"
+                    className="form-control"
+                    id="size"
+                    min="1"
+                    max="13"
+                    step="0.5"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="instagram">
+                    Instagram * (Expect to receive an invoice within two hours)
+                  </label>
+                  <input
+                    type="text"
+                    id="instagram"
+                    name="instagram"
+                    className="form-control"
+                  />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="notes">Notes</label>
+                  <textarea
+                    id="notes"
+                    name="notes"
+                    className="form-control"
+                    rows="5"
+                  />
+                </div>
+                <div className="form-group">
+                  <label>Upload a photo</label>
+                  <div className="custom-file">
+                    <input
+                      type="file"
+                      className="custom-file-input"
+                      id="attachments"
+                    />
+                    <label
+                      className="custom-file-label"
+                      htmlhtmlFor="attachments"
+                    >
+                      Choose file
+                    </label>
+                  </div>
+                </div>
+                <button type="submit" className="btn btn-secondary">
+                  Send Message
+                </button>
+              </form>
+            </div>
           </div>
-          <form>
-            <div className="form-group">
-              <label htmlhtmlFor="email">Email address</label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                name="email"
-                value={this.state.email}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlhtmlFor="model">Model (name of trainer)</label>
-              <input
-                type="text"
-                className="form-control"
-                id="model"
-                name="model"
-                value={this.state.model}
-                onChange={this.handleChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlhtmlFor="size">UK size</label>
-              <input
-                type="number"
-                className="form-control"
-                id="size"
-                min="1"
-                max="13"
-                step="0.5"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlhtmlFor="instagram">
-                Instagram (Expect to receive an invoice within two hours)
-              </label>
-              <input type="text" className="form-control" id="instagram" />
-            </div>
-            <div className="form-group">
-              <label htmlFor="notes">Additional notes</label>
-              <textarea id="notes" className="form-control" rows="4" />
-            </div>
-            <div className="form-group">
-              <label>Upload a photo</label>
-              <div className="custom-file">
-                <input
-                  type="file"
-                  className="custom-file-input"
-                  id="customFile"
-                />
-                <label className="custom-file-label" htmlhtmlFor="customFile">
-                  Choose file
-                </label>
+          <h3 className="mt-5">Recent Customers</h3>
+          <p className="text-gray">
+            Feel free to look through the full list{" "}
+            <a
+              href="https://www.instagram.com/stories/highlights/17927696740132330/"
+              target="_blank"
+              style={{ color: "#007bff" }}
+            >
+              here
+            </a>
+          </p>
+          <div className="row">
+            {reviews.map(review => (
+              <div className="col-6 col-sm-4 col-lg-3 mb-4">
+                <Img fluid={review.childImageSharp.fluid} alt="" />
               </div>
-            </div>
-            <button type="submit" className="btn btn-secondary w-100">
-              Send Request
-            </button>
-          </form>
+            ))}
+          </div>
         </div>
       </Layout>
     )
   }
 }
+
+export const sourcingImage = graphql`
+  fragment sourcingImage on File {
+    childImageSharp {
+      fluid(maxWidth: 1000) {
+        ...GatsbyImageSharpFluid
+      }
+    }
+  }
+`
+
+export const pageQuery = graphql`
+  query {
+    banner: file(relativePath: { eq: "banners/sourcing.jpg" }) {
+      childImageSharp {
+        fluid(maxWidth: 500) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+    img1: file(relativePath: { eq: "sourcing/1.PNG" }) {
+      ...sourcingImage
+    }
+    img2: file(relativePath: { eq: "sourcing/2.PNG" }) {
+      ...sourcingImage
+    }
+    img3: file(relativePath: { eq: "sourcing/3.PNG" }) {
+      ...sourcingImage
+    }
+    img4: file(relativePath: { eq: "sourcing/4.PNG" }) {
+      ...sourcingImage
+    }
+    img5: file(relativePath: { eq: "sourcing/5.PNG" }) {
+      ...sourcingImage
+    }
+    img7: file(relativePath: { eq: "sourcing/7.PNG" }) {
+      ...sourcingImage
+    }
+    img8: file(relativePath: { eq: "sourcing/8.PNG" }) {
+      ...sourcingImage
+    }
+    img9: file(relativePath: { eq: "sourcing/9.PNG" }) {
+      ...sourcingImage
+    }
+    img56: file(relativePath: { eq: "sourcing/56.PNG" }) {
+      ...sourcingImage
+    }
+    img3462: file(relativePath: { eq: "sourcing/3462.PNG" }) {
+      ...sourcingImage
+    }
+  }
+`
 
 export default PageComponent
